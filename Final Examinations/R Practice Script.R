8d95aca27d239d1f2e26ec0f1fec2a2229995ba6
therealdydx <wang.dong.yue05@gmail.com>
2020-11-11 21:07:50 +0800

    no comments

---
+++ Final Examinations/R Practice Script.R
@@ -0,0 +1,66 @@
+# Q1. READ DATA INTO SCRIPT
+
+advert <- read.csv("C:/Users/User/OneDrive/Documents/Yale-NUS/Quantitative Reasoning/Quantitative Reasoning Repository/Final Examinations/advert.csv")
+
+#___________________________________________________
+
+# Q2. COMPUTE SUMMARY STATISTICS FOR EACH OF THE QUANTITATIVE VARIABLES IN THE DATA FRAME
+
+
+
+#____________________________________________________
+
+# Q3. CREATE HISTOGRAMS FOR EACH OF THE QUANTITATIVE VARIABLES IN THE DATA FRAME
+
+
+
+#___________________________________________________
+
+# Q4. PLOT THE RELATIONSHIP BETWEEN BISCUIT SALES AND AGE
+
+
+
+#___________________________________________________
+
+# Q5. IS A LINEAR MODEL APPROPRIATE FOR THIS DATA?
+
+# IF YES, FIT THE LINEAR MODEL
+
+# EVALUATE IT BY LOOKING AT HISTOGRAM OF THE RESIDUALS AND A PLOT OF RESIDUALS VS FITTED VALUES
+
+# IF NO, EXPLAIN WHICH CONDITIONS ARE VIOLATED, AND DISCUSS IF ANYTHING CAN BE DONE TO FIX THIS
+
+#___________________________________________________
+
+# Q6. REPEAT THIS FOR ONLY MARKETS IN THE SOUTH
+
+#___________________________________________________
+
+# Q7. IS THERE A SIGNIFICANT DIFFERENCE IN INCOME BETWEEN NORTH AND SOUTH
+
+
+#___________________________________________________
+
+# Q8. WHAT PROPORTION OF MARKETS SPEND MORE ON INTERNET ADS THAN ON OTHER ADS?
+
+
+#___________________________________________________
+
+# Q9. WHAT PROPORTION OF MARKETS SPEND LESS ON TELEVISION ADS THAN ON OTHER ADS?
+
+#___________________________________________________
+
+# Q10. WHAT IS THE CORRELATION BETWEEN SPENDING ON RADIO ADS AND SPENDING ON NEWSPAPER ADS?
+
+# IS THIS CORRELATION SIGNIFICANTLY DIFFERENT FROM 0? HOW DO YOU KNOW?
+
+
+#___________________________________________________
+
+# FIT A LINEAR MODEL AS A FUNCTION OF AGE
+
+# ARE THE CONDITIONS OF LINEAR REGRESSION SATISFIED? HOW DO YOU KNOW?
+
+# IS THE SLOPE OF YOUR MODEL SIGNIFICANTLY DIFFERENT FROM 0? HOW DO YOU KNOW?
+
+
